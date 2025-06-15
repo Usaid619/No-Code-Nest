5a35995ac770f39d2ebb21b3d374ad26ead869b4
Shahzaman3 <faisalshahzaman8@gmail.com>
2025-06-15 06:43:37 +0530

    fixed

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import Hero from "./hero";
 import FeatureSection from "./FeatureSection";
 import Dragdrop from "./Dragdrop";
 import Automation from "./Automation";
 import Effort from "./Effort";
+import Testimonial from "./Testimonial";
 import Launch from "./Launch";
 import Footer from "./Footer";
 
 export default function main() {
       return (
             <>
+                  <Hero />
                   <FeatureSection />
                   <Dragdrop />
                   <Automation />
                   <Effort />
+                  <Testimonial />
                   <Launch />
                   <Footer />
             </>
